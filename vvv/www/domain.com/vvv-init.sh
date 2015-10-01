5e0ad53aceb09273744319f299f112f1bc903f1d
Joe Guilmette <joe.guilmette@gmail.com>
2015-10-01 11:05:50 +0700

    actually the worlds stupidest commit

--- vvv/www/domain.com/vvv-init.sh
+++ vvv/www/domain.com/vvv-init.sh
@@ -22,16 +22,16 @@ mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON domain.* TO root@local
 if [ ! -d wp-core ]
 
 then
-	echo "Installing WordPress using wp-cli"
+    echo "Installing WordPress using wp-cli"
 
-	mkdir wp-core
-	cd wp-core
-	
-	echo "Downloading WP Core"
-	wp core download 
-	
-	echo "Creating wp-config.php"
-	wp core config --dbname="domain" --dbuser=root --dbpass=root --dbhost="localhost" --dbprefix=wp_ --extra-php <<PHP
+    mkdir wp-core
+    cd wp-core
+    
+    echo "Downloading WP Core"
+    wp core download 
+    
+    echo "Creating wp-config.php"
+    wp core config --dbname="domain" --dbuser=root --dbpass=root --dbhost="localhost" --dbprefix=wp_ --extra-php <<PHP
 define( 'WP_DEBUG', true );
 
 /** WP Local Toolbox config */
@@ -39,36 +39,36 @@ define('WPLT_SERVER','local');
 define('WPLT_ADMINBAR','always');
 define('WPLT_AIRPLANE','true');
 PHP
-	
-	echo "Installing WordPress"
-	wp core install --title="Just another VVV install" --url=domain.dev --admin_user=admin --admin_password=password --admin_email=admin@email.com
-	
-	echo "Installing Plugins"
-	# You can install other plugins with wp-cli using this command:
-	# wp plugin install wp-local-toolbox
-	# or
-	# wp plugin install wordpress-seo
+    
+    echo "Installing WordPress"
+    wp core install --title="Just another VVV install" --url=domain.dev --admin_user=admin --admin_password=password --admin_email=admin@email.com
+    
+    echo "Installing Plugins"
+    # You can install other plugins with wp-cli using this command:
+    # wp plugin install wp-local-toolbox
+    # or
+    # wp plugin install wordpress-seo
 
-	# Install WPLT
-	# It needs to be run as an mu-plugin for the plugin disabling
-	# feature to work. Read more here: https://github.com/joeguilmette/wp-local-toolbox
-	mkdir wp-content/mu-plugins
-	curl -sS https://downloads.wordpress.org/plugin/wp-local-toolbox.zip > wp-content/mu-plugins/wp-local-toolbox.zip
-	unzip -q wp-content/mu-plugins/wp-local-toolbox.zip -d wp-content/mu-plugins/
-	rm wp-content/mu-plugins/wp-local-toolbox.zip
+    # Install WPLT
+    # It needs to be run as an mu-plugin for the plugin disabling
+    # feature to work. Read more here: https://github.com/joeguilmette/wp-local-toolbox
+    mkdir wp-content/mu-plugins
+    curl -sS https://downloads.wordpress.org/plugin/wp-local-toolbox.zip > wp-content/mu-plugins/wp-local-toolbox.zip
+    unzip -q wp-content/mu-plugins/wp-local-toolbox.zip -d wp-content/mu-plugins/
+    rm wp-content/mu-plugins/wp-local-toolbox.zip
 
-	mv wp-content/mu-plugins/wp-local-toolbox/toolbox/ wp-content/mu-plugins/toolbox/
-	mv wp-content/mu-plugins/wp-local-toolbox/wplt-loader.php wp-content/mu-plugins/wplt-loader.php
-	mv wp-content/mu-plugins/wp-local-toolbox/read* wp-content/mu-plugins/toolbox/
-	mv wp-content/mu-plugins/wp-local-toolbox/LICENSE wp-content/mu-plugins/toolbox/LICENSE
-	rm -rf wp-content/mu-plugins/wp-local-toolbox/
-	# I'm pretty sure we sent a man to the moon with fewer lines of code
-	# than it takes me to unpack a zip file. Whatever. It works.
+    mv wp-content/mu-plugins/wp-local-toolbox/toolbox/ wp-content/mu-plugins/toolbox/
+    mv wp-content/mu-plugins/wp-local-toolbox/wplt-loader.php wp-content/mu-plugins/wplt-loader.php
+    mv wp-content/mu-plugins/wp-local-toolbox/read* wp-content/mu-plugins/toolbox/
+    mv wp-content/mu-plugins/wp-local-toolbox/LICENSE wp-content/mu-plugins/toolbox/LICENSE
+    rm -rf wp-content/mu-plugins/wp-local-toolbox/
+    # I'm pretty sure we sent a man to the moon with fewer lines of code
+    # than it takes me to unpack a zip file. Whatever. It works.
 
-	echo "Goodbye Dolly"
-	wp plugin uninstall hello
-	wp plugin uninstall akismet
-	cd ..
+    echo "Goodbye Dolly"
+    wp plugin uninstall hello
+    wp plugin uninstall akismet
+    cd ..
 fi
 
 # The Vagrant site setup script will restart Nginx for us
